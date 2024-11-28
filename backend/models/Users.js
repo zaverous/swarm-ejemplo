ab94e939e13a165ea05b59f3590b41d957f738ac
adrmurbau <adrianmb41@gmail.com>
2024-11-28 21:28:00 +0100

    Prototipo de Chat grupal en vivo

---
+++ backend/models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // Contraseña encriptada
+});
+
+// Antes de guardar un usuario, hashear la contraseña
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('Users', userSchema);
